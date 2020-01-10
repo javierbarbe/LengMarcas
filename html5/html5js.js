c9bd18a5aa6e75cd0fff270c13b8aa2174477c6f
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2020-01-10 19:19:17 +0100

    sistemas

---
+++ html5/html5js.js
@@ -0,0 +1,63 @@
+function compruebaTo(){
+    var nomComp=0;
+    var nombre= document.getElementById('nombre').value;
+    if (nombre.length>0){
+         nomComp=1;
+    }else{
+        alert('por favor Nombre no puede quedar vacio');
+        document.getElementById('user').focus();
+    }
+    var ape1Comp=0;
+    var ape1= document.getElementById('apellido1').value;
+    if(ape1.length>0){
+        ape1Comp=1;
+    }else{
+        alert('por favor Apellido1 no puede quedar vacio');
+    }
+    var ape2Comp=0;
+    var ape2= document.getElementById('apellido2').value;
+    if(ape2.length>0){
+        ape2Comp=1;
+    }else{
+        alert('por favor Apellido2 no puede quedar vacio');
+    }
+    
+
+/* var Comp=0;
+    var contra= document.getElementById('pass1').value;
+    if(contra.length>0){
+        contraComp=1;
+    }else{
+        alert('por favor Contraseña no puede quedar vacio');
+    }
+    var contra2=document.getElementById('pass2').value;
+    if(contra2!=contra){
+        alert('Las contraseñas no coinciden');
+        contraComp=0;
+        document.getElementById('pass2').focus();
+    }
+    var correoComp=0;
+    var correo= document.getElementById('correo').value;
+    if(correo.length>0){
+        correoComp=1;
+    }else{
+        alert('Por favor introduzca correo');
+    }
+    */
+
+}// fin compruebaTo()
+
+function pon_submenu(){
+       
+    var cocheSi=document.getElementById('cocheSi').checked;
+    var carnetSi=document.getElementById('carnetSi').checked;
+    
+    if (carnetSi==true){
+            document.getElementById('menuOculto2').style.display = "contents";
+    } 
+    if (carnetSi==true){
+          document.getElementById('menuOculto').style.display = "contents";
+    } 
+   
+    
+}
