ae55733e7f532eeedbf70d058554877222cab09b
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2020-08-31 18:09:44 +0200

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -1,60 +1,60 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    myFunction();
-})
+// document.addEventListener("DOMContentLoaded", () => {
+//   myFunction();
+// });
 
-function comenzar() {
-  zonadatos = document.getElementsByClassName("_3-cMa _3Whw5")[0].innerHTML;
-  navigator.webkitPersistentStorage.requestQuota(5 * 1024 * 1024, acceso);
-  console.log("comenzamos!!");
-  console.log(horas);
+// function comenzar() {
+//   zonadatos = document.getElementsByClassName("_3-cMa _3Whw5")[0].innerHTML;
+//   navigator.webkitPersistentStorage.requestQuota(5 * 1024 * 1024, acceso);
+//   console.log("comenzamos!!");
+//   console.log(horas);
 
-  ////////////////////////////////////////////////////////
-  // clase span NOMBRE "_2FVVk _2UL8j""
-}
-document
-  .getElementsByClassName("_2FVVk _2UL8j")[0]
-  .addEventListener("click", muestraArray, null);
-document
-  .getElementsByClassName("_3ee1T _1LkpH")[0]
-  .addEventListener("click", muestraArray, null);
-function muestraArray() {
-  console.log(horas);
+//   ////////////////////////////////////////////////////////
+//   // clase span NOMBRE "_2FVVk _2UL8j""
+// }
+// document
+//   .getElementsByClassName("_2FVVk _2UL8j")[0]
+//   .addEventListener("click", muestraArray, null);
+// document
+//   .getElementsByClassName("_3ee1T _1LkpH")[0]
+//   .addEventListener("click", muestraArray, null);
+// function muestraArray() {
+//   console.log(horas);
 
-  document
-    .getElementsByClassName("_3ee1T _1LkpH")[0]
-    .classList.add("ojeteMorenote");
-}
-function acceso() {
-  window.webkitRequestFileSystem(
-    PERSISTENT,
-    5 * 1024 * 1024,
-    crearsis,
-    errores
-  );
-}
-function crearsis(sistema) {
-  espacio = sistema.root;
-}
+//   document
+//     .getElementsByClassName("_3ee1T _1LkpH")[0]
+//     .classList.add("ojeteMorenote");
+// }
+// function acceso() {
+//   window.webkitRequestFileSystem(
+//     PERSISTENT,
+//     5 * 1024 * 1024,
+//     crearsis,
+//     errores
+//   );
+// }
+// function crearsis(sistema) {
+//   espacio = sistema.root;
+// }
 
-function crear() {
-  if (horas.length > 0) {
-    espacio.getFile(
-      "horas conectadas.txt",
-      { create: true, excluisve: false },
-      mostrar,
-      errores
-    );
-  }
-}
+// function crear() {
+//   if (horas.length > 0) {
+//     espacio.getFile(
+//       "horas conectadas.txt",
+//       { create: true, excluisve: false },
+//       mostrar,
+//       errores
+//     );
+//   }
+// }
 
-function errores(error) {
-  console.log(error.code);
-}
-function mostrar(entrada) {
-  document.getElementById("entrada").innerHTML = "";
-  document.getElementById("entrada").innerHTML = "EXITO !!";
-  document.getElementById("entrada").innerHTML += "Nombre: " + entrada.name;
-}
+// function errores(error) {
+//   console.log(error.code);
+// }
+// function mostrar(entrada) {
+//   document.getElementById("entrada").innerHTML = "";
+//   document.getElementById("entrada").innerHTML = "EXITO !!";
+//   document.getElementById("entrada").innerHTML += "Nombre: " + entrada.name;
+// }
 var primerElemento;
 var ultimoElemento;
 var espacio;
@@ -74,31 +74,30 @@ var fechaLimite = new Date(2021, 07, 31, 14, 52, 0);
 function myFunction() {
   if (salir == false) {
     setInterval(alertFunc, 2200);
- 
   } else {
     console.log("FINITO ENCANTATEM");
   }
 }
 function alertFunc() {
   if (parseInt(fecha.getTime()) > parseInt(fechaLimite.getTime())) {
-    
+    //console.log("es menor la hora");
     salir = true;
     stop();
   } else {
     var estado = "";
     fecha = new Date();
-    
+    // console.log(typeof fecha.getTime());
+    // console.log(typeof fechaLimite.getTime());
     els = document.getElementsByClassName("_3-cMa _3Whw5");
     // console.log(els);
     for (let i = 0; i < els.length; i++) {
       estado = els[i].innerText;
+      //  console.log(estado+" estoy hasta la polla del estado copon");
       segundoActual = parseInt(fecha.getSeconds());
-     
-        //  estados.push(estado);
-      
+
+      //  estados.push(estado);
 
       switch (estado) {
-
         case "en línea":
           hora =
             fecha.getHours() +
@@ -132,24 +131,25 @@ function alertFunc() {
         case "desconectado":
           horas.push("fin");
           console.log("fin de conexion");
-          if (terminar == false) {   
+          if (terminar == false) {
             terminar = true;
           }
           break;
       }
     }
-    if (els.length== 0){
-     //   console.log("no estas en linea");
-       // estados.push("desconectado");
-        if(horas[horas.length-1] != "fin" ){
-            for( var i = 0; i<horas.length-1; i++){
-                if (i!= 0){
-                    horas.push("fin");
-                }
-            }
+    if (els.length == 0) {
+      //   console.log("no estas en linea");
+      // estados.push("desconectado");
+      if (horas[horas.length - 1] != "fin") {
+        for (var i = 0; i < horas.length - 1; i++) {
+          if (i != 0) {
+            horas.push("fin");
+          }
         }
+      }
     }
 
+    //console.log(ano + " ano vale eso");
   }
   if (ano == true) {
     /// AÑADIENDO EL ENLACE QUE GENERE EL DOC////////////////////
@@ -173,9 +173,11 @@ function formateaHoras(arrayHoras) {
       if (primerElemento == null) {
         primerElemento = 0;
       }
-     
+      // console.log(i + " valor del index cuando el estado es fin");
       ultimoElemento = i - 1;
-    
+      //   for ( var i =  primerElemento; i<ultimoElemento; i++){
+      //       console.log(arrayHoras[i]);
+      //   }
       console.log(arrayHoras[primerElemento] + " comienzo de conexion");
       console.log(arrayHoras[ultimoElemento] + " fin de conexion");
       primerElemento = ultimoElemento + 2;
@@ -190,11 +192,34 @@ function formateaHoras(arrayHoras) {
     }
   }
 
+  /////
 
+  // arrayHoras.forEach((element, index) => {
+  //     if (element == "fin" && index!= 0) {
+  //         if (primerElemento==null) {primerElemento=0;}
+  //         console.log(index+ " valor del index cuando el estado es fin" );
+  //         ultimoElemento= index -1;
+  //       //   for ( var i =  primerElemento; i<ultimoElemento; i++){
+  //       //       console.log(arrayHoras[i]);
+  //       //   }
+  //       console.log(arrayHoras[primerElemento]+ " comienzo de conexion");
+  //       console.log(arrayHoras[ultimoElemento]+ " fin de conexion");
+  //         primerElemento= ultimoElemento+2;
+  //     }
+  //      if(element.match('.* (grabando audio)')){
+  //         console.log("coincide el elemento con grabando audio, indice "+index);
+  //         ultimoElemento= index;
+  //         grabando = true;
+  //     }
+  //     if (index == arrayHoras.length-1){
+  //         console.log(arrayHoras[arrayHoras.length-1]+ " fin de conexion");
+  //       }
+  //   });
 
-  var primerElemento=0;
+  var primerElemento = 0;
   var ultimoElemento;
   var grabando = false;
 
-
+  //////////
 }
+
